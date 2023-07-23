b58bfc15afcda5547711ca1152afeca34cfb8da7
Brandon Dykun <brandon.dykun@gmail.com>
2023-07-23 00:03:57 -0600

    Added loading indicators fir Iframe and mdl page.

--- app/mdl/loading.tsx
+++ app/mdl/loading.tsx
@@ -1,5 +1,11 @@
+import { FadeLoader } from "react-spinners";
+
 const Loading = () => {
-  return <div className="text-stone-300">Loading...</div>;
+  return (
+    <div className="text-stone-300 text-3xl flex justify-center items-center min-h-screen">
+      <FadeLoader color="#fbbf24" />
+    </div>
+  );
 };
 
 export default Loading;
--- components/Iframe.tsx
+++ components/Iframe.tsx
@@ -1,6 +1,8 @@
 "use client";
 
 import { useEffect, useState } from "react";
+import { Youtube } from "lucide-react";
+import { BarLoader } from "react-spinners";
 
 const Iframe = ({ ...rest }) => {
   const [windowWidth, setWindowWidth] = useState(200);
@@ -23,6 +25,9 @@ const Iframe = ({ ...rest }) => {
   });
 
   useEffect(() => {
+    console.log("IN THE USE EFFECT");
+    console.log("window: ", window);
+
     if (window !== undefined) {
       setWindowExists(true);
       if (window.innerWidth < 400) {
@@ -37,7 +42,15 @@ const Iframe = ({ ...rest }) => {
 
   return (
     <>
-      {!windowExists ? null : (
+      {!windowExists ? (
+        <div className="h-[143px] w-[250px] flex justify-center items-center bg-stone-800">
+          <div className="flex flex-col gap-2 justify-center items-center">
+            <Youtube size={50} />
+            <div>Loading Video...</div>
+            <BarLoader color="#d6d3d1" />
+          </div>
+        </div>
+      ) : (
         <iframe
           {...rest}
           width={calculatedWidth}
